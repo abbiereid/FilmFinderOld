206cd4c92c2b96fd922e950dac61633ed2182895
abbie <abbiereid222@gmail.com>
2024-04-14 22:58:23 +0100

    .

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ window.onload = function() {
         event.preventDefault();
         searchMovies(searchBar.value);
     });
+
+    
 };
 
 
@@ -20,23 +22,30 @@ const options = {
 
 function searchMovies(query) {
     const results = document.querySelector('.results');
+      
 
     const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
 
     fetch(url, options)
         .then(response => response.json())
         .then(response => {
-            const movies = response.results.slice(0, 5);
-
-            movies.map((movie, index) => {
+            response.results.map((movie) => {
                 const movieDiv = document.createElement('div');
                 movieDiv.classList.add('movie');
             
-                const moviePoster = document.createElement('img');
-                moviePoster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+                const movieImage = new Image();
+                movieImage.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
             
-                movieDiv.appendChild(moviePoster);
+                movieImage.onload = function() {
+                    movieDiv.appendChild(movieImage);
+                }
                 results.appendChild(movieDiv);
             });
+
+            var flkty = new Flickity( results, {
+                contain: true,
+                wrapAround: true
+            });
+
         });
 }
