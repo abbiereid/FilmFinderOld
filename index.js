5bed038b1e0dd0554371496400bc279562764539
abbie <abbiereid222@gmail.com>
2024-04-27 18:34:12 +0100

    .

--- index.js
+++ index.js
@@ -42,7 +42,26 @@ function searchMovies(query) {
                 const movieImage = new Image();
                 movieImage.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
                 movieImage.alt = movie.title;
-                movieImage.classList.add('clickable');
+
+                /*const svgNS = "http://www.w3.org/2000/svg";
+                const info = document.createElementNS(svgNS, 'svg');
+                info.setAttributeNS(null, 'viewBox', '0 0 512 512');
+                
+                const path = document.createElementNS(svgNS, 'path');
+                path.setAttributeNS(null, 'd', 'M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z');
+                
+                info.appendChild(path);
+                info.classList.add('info');
+                //info.classList.add('hidden');
+                info.classList.add('clickable');
+
+                movieDiv.appendChild(info);*/
+
+                const expand = document.createElement('p');
+                expand.innerText = 'Double click for more information';
+                expand.classList.add('expand');
+
+                movieDiv.appendChild(expand);
                 
                 movieDiv.appendChild(movieImage);
                 results.appendChild(movieDiv);
