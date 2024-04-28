31479b282f331f010d17fb9d3929373d566dc39c
abbie <abbiereid222@gmail.com>
2024-04-28 21:58:07 +0100

    adding 'dblclick' alternative for touch screen

--- index.js
+++ index.js
@@ -66,94 +66,113 @@ function searchMovies(query) {
                 };
 
                 movieDiv.addEventListener('dblclick', function() {
-                    results.classList.toggle('hidden');
-                    expanded.classList.toggle('hidden');
-
-                    const searchDiv = document.querySelector('.menu');
-                    searchDiv.classList.toggle('hidden');
-
-                    const imageSection = document.querySelector('.expandedImage');
-                    const textSection = document.querySelector('.expandedText');
-
-                    imageSection.innerHTML = '';
-                    textSection.innerHTML = '';
-
-                    const expandedImage = new Image();
-                    expandedImage.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-                    expandedImage.alt = movie.title;
-
-                    const expandedTitle = document.createElement('h1');
-                    expandedTitle.textContent = movie.title;
-
-                    const hr = document.createElement('hr');
-
-                    const expandedOverview = document.createElement('p');
-                    expandedOverview.textContent = movie.overview;
-
-                    const date = document.createElement('h3');
-                    date.textContent = `Release Date: ${movie.release_date}`;
-
-                    const rating = movie.vote_average * 10;
-
-                    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-                    svg.classList.add("rating");
-                    svg.setAttribute("viewBox", "0 0 36 36");
-                    svg.style.transform = "rotate(-90deg)";
-                    
-                    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-                    circle.setAttribute("class", "circle");
-                    circle.setAttribute("stroke", "grey");
-                    circle.setAttribute("fill", "transparent");
-                    circle.setAttribute("r", "16");
-                    circle.setAttribute("cx", "18");
-                    circle.setAttribute("cy", "18");
-                    circle.setAttribute("stroke-width", "3");
-                    svg.appendChild(circle);
-                    
-                    const progress = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-                    progress.setAttribute("class", "progress");
-                    progress.setAttribute("fill", "transparent");
-                    progress.setAttribute("r", "16");
-                    progress.setAttribute("cx", "18");
-                    progress.setAttribute("cy", "18");
-                    progress.setAttribute("stroke-dasharray", `${rating}, 100`);
-                    progress.setAttribute("stroke-width", "3");
-                    
-                    if (rating >= 70) {
-                        progress.setAttribute("stroke", "green");
-                    } else if (rating >= 40) {
-                        progress.setAttribute("stroke", "orange");
-                    } else {
-                        progress.setAttribute("stroke", "red");
-                    }
-                    
-                    svg.appendChild(progress);
-                    
-                    const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-                    text.setAttribute("x", "18");
-                    text.setAttribute("y", "20");
-                    text.setAttribute("text-anchor", "middle");
-                    text.textContent = rating + "%";
-                    text.style.fontSize = "40%";
-                    text.style.transform = "rotate(90deg)";
-                    text.style.transformOrigin = "center";
-                    svg.appendChild(text);
-
-                    imageSection.appendChild(expandedImage);
-                    textSection.appendChild(expandedTitle);
-                    textSection.appendChild(hr);
-                    textSection.appendChild(expandedOverview);
-                    textSection.appendChild(date);
-                    textSection.appendChild(svg);
-
-                    const backButton = document.querySelector('#backButton');
-                    backButton.addEventListener('click', function() {
-                        results.classList.toggle('hidden');
-                        expanded.classList.toggle('hidden');
-                        searchDiv.classList.toggle('hidden');
-                    });
-
+                    expandMovie(movie);
                 });
+
+                let lastTouchTime = 0;
+
+                movieDiv.addEventListener('touchstart', function (event) {
+                    const currentTime = new Date().getTime();
+                    const tapLength = currentTime - lastTouchTime;
+                    lastTouchTime = currentTime;
+
+                    if (tapLength < 500 && tapLength > 0) {
+                        expandMovie(movie);
+                        event.preventDefault();
+                    }
+                });
+
             });
         });
+}
+
+function expandMovie(movie) {
+    const results = document.querySelector('.results');
+    const expanded = document.querySelector('.expanded');
+    results.classList.toggle('hidden');
+    expanded.classList.toggle('hidden');
+
+    const searchDiv = document.querySelector('.menu');
+    searchDiv.classList.toggle('hidden');
+
+    const imageSection = document.querySelector('.expandedImage');
+    const textSection = document.querySelector('.expandedText');
+
+    imageSection.innerHTML = '';
+    textSection.innerHTML = '';
+
+    const expandedImage = new Image();
+    expandedImage.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    expandedImage.alt = movie.title;
+
+    const expandedTitle = document.createElement('h1');
+    expandedTitle.textContent = movie.title;
+
+    const hr = document.createElement('hr');
+
+    const expandedOverview = document.createElement('p');
+    expandedOverview.textContent = movie.overview;
+
+    const date = document.createElement('h3');
+    date.textContent = `Release Date: ${movie.release_date}`;
+
+    const rating = movie.vote_average * 10;
+
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.classList.add("rating");
+    svg.setAttribute("viewBox", "0 0 36 36");
+    svg.style.transform = "rotate(-90deg)";
+
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute("class", "circle");
+    circle.setAttribute("stroke", "grey");
+    circle.setAttribute("fill", "transparent");
+    circle.setAttribute("r", "16");
+    circle.setAttribute("cx", "18");
+    circle.setAttribute("cy", "18");
+    circle.setAttribute("stroke-width", "3");
+    svg.appendChild(circle);
+
+    const progress = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    progress.setAttribute("class", "progress");
+    progress.setAttribute("fill", "transparent");
+    progress.setAttribute("r", "16");
+    progress.setAttribute("cx", "18");
+    progress.setAttribute("cy", "18");
+    progress.setAttribute("stroke-dasharray", `${rating}, 100`);
+    progress.setAttribute("stroke-width", "3");
+
+    if (rating >= 70) {
+        progress.setAttribute("stroke", "green");
+    } else if (rating >= 40) {
+        progress.setAttribute("stroke", "orange");
+    } else {
+        progress.setAttribute("stroke", "red");
+    }
+
+    svg.appendChild(progress);
+
+    const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    text.setAttribute("x", "18");
+    text.setAttribute("y", "20");
+    text.setAttribute("text-anchor", "middle");
+    text.textContent = rating + "%";
+    text.style.fontSize = "40%";
+    text.style.transform = "rotate(90deg)";
+    text.style.transformOrigin = "center";
+    svg.appendChild(text);
+
+    imageSection.appendChild(expandedImage);
+    textSection.appendChild(expandedTitle);
+    textSection.appendChild(hr);
+    textSection.appendChild(expandedOverview);
+    textSection.appendChild(date);
+    textSection.appendChild(svg);
+
+    const backButton = document.querySelector('#backButton');
+    backButton.addEventListener('click', function() {
+        results.classList.toggle('hidden');
+        expanded.classList.toggle('hidden');
+        searchDiv.classList.toggle('hidden');
+    });
 }
