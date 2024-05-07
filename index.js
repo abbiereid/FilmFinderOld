7366d4e85fa8242279a9d3e7a172953ad6b017fc
abbie <abbiereid222@gmail.com>
2024-05-07 01:28:35 +0100

    .

--- index.js
+++ index.js
@@ -37,6 +37,21 @@ window.onload = function() {
             needTo.textContent = "Already have an account? Click here to Login";
         }
     });
+
+    document.querySelector('#registerForm').addEventListener('submit', async function(e) {
+        e.preventDefault();
+        
+        await fetch('register.php', {
+            method: 'POST',
+            body: new FormData(e.target)
+         }).then(response => {
+             if (response.success === true) {
+                 alert('Registration successful!');
+             } else {
+                 alert('Error: ' + response.status);
+             }
+         });
+     });
 };
 
 
