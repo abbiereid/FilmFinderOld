502e11266e0bb9fbf8c5f0f601f509a31277d7dd
abbie <abbiereid222@gmail.com>
2024-05-07 01:44:06 +0100

    .

--- index.js
+++ index.js
@@ -41,17 +41,16 @@ window.onload = function() {
     document.querySelector('#registerForm').addEventListener('submit', async function(e) {
         e.preventDefault();
         
-        await fetch('register.php', {
+        fetch('register.php', {
             method: 'POST',
             body: new FormData(e.target)
-         }).then(response => {
-             if (response.success === true) {
-                 alert('Registration successful!');
-             } else {
-                 alert('Error: ' + response.status);
-             }
-         });
-     });
+        })
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => console.error('Error:', error));
+    });
 };
 
 
